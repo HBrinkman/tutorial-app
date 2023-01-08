db289ece8882c291f78b89b92d4f57c945b11247
Henry Brinkman <Henry.Brinkman@topicus.nl>
2023-01-08 19:54:10 +0100

    Step-23 en firestore iOS fix

--- lib/views/notes/notes_view.dart
+++ lib/views/notes/notes_view.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart' show ReadContext;
 import 'package:tutorial_app/constants/routes.dart';
 import 'package:tutorial_app/enums/menu_action.dart';
 import 'package:tutorial_app/services/auth/auth_service.dart';
+import 'package:tutorial_app/services/auth/bloc/auth_bloc.dart';
+import 'package:tutorial_app/services/auth/bloc/auth_event.dart';
 import 'package:tutorial_app/services/cloud/cloud_note.dart';
 import 'package:tutorial_app/services/cloud/firebase_cloud_storage.dart';
 import 'package:tutorial_app/utilities/dialogs/logout_dialog.dart';
@@ -42,11 +45,9 @@ class _NotesViewState extends State<NotesView> {
                 case MenuAction.logout:
                   final shouldLogout = await showLogoutDialog(context);
                   if (shouldLogout) {
-                    await AuthService.firebase().logOut();
-                    Navigator.of(context).pushNamedAndRemoveUntil(
-                      loginRoute,
-                      (_) => false,
-                    );
+                    context.read<AuthBloc>().add(
+                          const AuthEventLogout(),
+                        );
                   }
                   break;
               }
