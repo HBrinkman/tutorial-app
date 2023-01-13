afda3444fa79b9b0597e608105d4d978721d4854
Henry Brinkman <Henry.Brinkman@topicus.nl>
2023-01-13 12:09:27 +0100

    Step-25

--- lib/main.dart
+++ lib/main.dart
@@ -20,13 +20,9 @@ void main() {
       ),
       home: BlocProvider<AuthBloc>(
         create: (context) => AuthBloc(FirebaseAuthProvider()),
-        child: HomePage(),
+        child: const HomePage(),
       ),
       routes: {
-        loginRoute: (context) => const LoginView(),
-        registerRoute: (context) => const RegisterView(),
-        notesRoute: (context) => const NotesView(),
-        verifyEmailRoute: (context) => const VerifyEmailView(),
         createOrUpdateNoteRoute: (context) => const CreateUpdateNoteView(),
       }));
 }
@@ -41,9 +37,11 @@ class HomePage extends StatelessWidget {
       if (state is AuthStateLoggedIn) {
         return const NotesView();
       } else if (state is AuthStateNeedsVerification) {
-        return VerifyEmailView();
+        return const VerifyEmailView();
       } else if (state is AuthStateLoggedOut) {
         return const LoginView();
+      } else if (state is AuthStateRegistering) {
+        return const RegisterView();
       } else {
         return const Scaffold(
           body: CircularProgressIndicator(),
--- lib/services/cloud/cloud_storage_exceptions.dart
+++ lib/services/cloud/cloud_storage_exceptions.dart
@@ -1,5 +1,3 @@
-import 'package:flutter/material.dart';
-
 class CloudStorageException implements Exception {
   const CloudStorageException();
 }
