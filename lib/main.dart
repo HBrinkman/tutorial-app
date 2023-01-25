f754feefec75e366bb4d8d15ff3c750b932bf8e0
Henry Brinkman <Henry.Brinkman@topicus.nl>
2023-01-25 16:17:55 +0100

    Step-27

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:tutorial_app/services/auth/bloc/auth_bloc.dart';
 import 'package:tutorial_app/services/auth/bloc/auth_event.dart';
 import 'package:tutorial_app/services/auth/bloc/auth_state.dart';
 import 'package:tutorial_app/services/auth/firebase_auth_provider.dart';
+import 'package:tutorial_app/views/forgot_password_view.dart';
 import 'package:tutorial_app/views/login_view.dart';
 import 'package:tutorial_app/views/notes/create_update_note_view.dart';
 import 'package:tutorial_app/views/notes/notes_view.dart';
@@ -50,6 +51,8 @@ class HomePage extends StatelessWidget {
         return const VerifyEmailView();
       } else if (state is AuthStateLoggedOut) {
         return const LoginView();
+      } else if (state is AuthStateForgotPassword) {
+        return const ForgotPasswordView();
       } else if (state is AuthStateRegistering) {
         return const RegisterView();
       } else {
